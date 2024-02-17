7c69ba07fc0737612188c9ae2aaea020d88e1cee
Shaunnn_M <mkntha093@myuct.ac.za>
2024-02-17 23:23:20 +0200

    header file for Counter.cpp

---
+++ Counter.h
@@ -0,0 +1,17 @@
+//
+// Created by Thabanhg Mokoena on 2024/02/16.
+//
+
+#ifndef WORDCOUNTER_COUNTER_H
+#define WORDCOUNTER_COUNTER_H
+#include <string>
+
+namespace MKNTHA093{
+
+    int countWords(const std::string &line);
+
+    int countLines();
+
+    int countCharacters(const std::string &line);
+}
+#endif //WORDCOUNTER_COUNTER_H
